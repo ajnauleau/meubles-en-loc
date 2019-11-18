a1dc2823d29d9cf84709aeb26d8a7394c0b54560
Antoine Nauleau <ajnauleau@gmail.com>
2019-11-18 17:20:09 +0100

    Controller

---
+++ app/controllers/meubles_controller.rb
@@ -0,0 +1,19 @@
+class MeublesController < ApplicationController
+
+  def index
+      @meubles = Meubles.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+end
---
+++ test/controllers/meubles_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MeublesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
