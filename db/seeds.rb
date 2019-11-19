bd811effd37c55d8cfa55c0aa28710db2f7624ed
bosc kevin <kevin_bosc@hotmail.fr>
2019-11-19 14:13:51 +0100

    error à ne pas push dans master

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Meuble.destroy_all
+meubles = Meuble.create! ([{
+  name: "Baignoire",
+  category: "Bathroom",
+  description: "elle prend l'eau",
+  height: 10,
+  width: 10,
+  length: 10,
+  daily_rate: 10
+ },
+ {
+  name: "toilette",
+  category: "Bathroom",
+  description: "elle fuit",
+  height: 10,
+  width: 10,
+  length: 10,
+  daily_rate: 5
+}])
