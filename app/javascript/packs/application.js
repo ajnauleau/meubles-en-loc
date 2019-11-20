93782908d53a0d3379e0220109898aceb21b514a
bosc kevin <kevin_bosc@hotmail.fr>
2019-11-20 15:49:13 +0100

    update calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+import flatpickr from 'flatpickr'
+
+import "../plugins/flatpickr"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+import 'flatpickr/dist/flatpickr.min.css'
+
+
+flatpickr(".datepicker", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
