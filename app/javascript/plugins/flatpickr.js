f260843cb2b17003606a84fc40ebb7b218379817
Antoine Nauleau <ajnauleau@gmail.com>
2019-11-21 16:40:03 +0100

    Calendar and picture working

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,9 +1,9 @@
-// import flatpickr from "flatpickr"
-// import "flatpickr/dist/flatpickr.min.css"
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-// flatpickr("#range_start", {
-//   altInput: true,
-//   plugins: [new rangePlugin({ input: "#range_end"})]
-// })
+flatpickr("#range_start", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
 
