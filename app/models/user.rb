583cbf0b4b41f519bab21eeb2583cfaeb4a0cf76
Alexia Jolly <alexia.jolly@hotmail.com>
2019-11-21 16:20:29 +0100

    dashboard work in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :meubles, dependent: :destroy
   has_many :locations, dependent: :destroy
+  has_many :booked_meubles, through: :locations, source: :meuble
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  # def get_reservations
+  #   self.meubles.map { |m| m.locations }.flatten
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/profil', to: 'dashboards#profil'
   resources :meubles, only:[:index, :show, :new, :create] do
     resources :locations, only: [:create]
 
     collection do
       post 'filter', to: 'meubles#filter'
     end
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
