519bc8c2ca84c346868d9014ab0aed137367beae
Antoine Nauleau <ajnauleau@gmail.com>
2019-11-22 11:50:42 +0100

    space-around

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
 
-def profil
+def profile
   @meubles = current_user.meubles
   @locations = current_user.locations
   @booked_meubles = current_user.booked_meubles
